ea9c5e32cfc13be36164cf758523e4dd31b2d85e
Djordje <djordjen7@gmail.com>
2025-11-08 23:10:31 +0100

    Brisanje nepotrebnih fajlova

--- scripts/script3860.js
+++ scripts/script3860.js
@@ -389,27 +389,6 @@
     $(function () {
       $('[data-toggle="tooltip"]').tooltip();
     });
-    
-    $(document).ready(function(){
-      $('.slick-slide-container').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: true,
-        speed: 300,
-        responsive: [
-        {
-          breakpoint: 800,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-      });
-    });
+
 })(window.jQuery);
 
